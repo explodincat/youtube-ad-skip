44ea254d0a58dcb0fc6a5e4bfabfbe2e9567bd6c
explodincat <147451900+explodincat@users.noreply.github.com>
2024-07-22 22:16:53 -0700

    Update ad-skip.js

--- ad-skip.js
+++ ad-skip.js
@@ -1,13 +1,3 @@
-// ==UserScript==
-// @name         自动跳过YouTube广告
-// @namespace    youtube
-// @version      1.0
-// @description  在YouTube网页上自动跳过广告
-// @author       Joey Gambler
-// @match        *://www.youtube.com/*
-// @grant        none
-// ==/UserScript==
-
 (function () {
     'use strict';
 
@@ -19,6 +9,6 @@
         }
     }
 
-    // 设置检测时间间隔
-    var timer = setInterval(skipAd, 1000); // 1000毫秒 = 1秒
+    
+    var timer = setInterval(skipAd, 0); 
 })();
